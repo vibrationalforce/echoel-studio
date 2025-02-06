34def35771fe3086ef2f50aaad5267c0ca6ef084
vibrationalforce <82265317+vibrationalforce@users.noreply.github.com>
2025-02-06 18:13:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,23 +16,9 @@ form.addEventListener('submit', (event) => {
     return;
   }
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
-
-  $to = "michael@tropicaldrones.com";
-  $subject = "Neue Nachricht von deiner Website";
-  $headers = "From: $email";
-
-  if (mail($to, $subject, $message, $headers)) {
-    echo "Vielen Dank für deine Nachricht!";
-  } else {
-    echo "Beim Senden deiner Nachricht ist ein Fehler aufgetreten.";
-  }
-}?>
-
+  // Hier den Code zum Senden des Formulars einfügen.
+  // Zum Beispiel:
+  // form.submit();
 
   alert('Vielen Dank für Ihre Nachricht!');
   form.reset();
